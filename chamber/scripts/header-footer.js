6cebec524adbf68688703568fe00ae8ac235e7c6
Peyton White <whi20027@byui.edu>
2024-05-16 15:28:19 -0600

    js

---
+++ chamber/scripts/header-footer.js
@@ -0,0 +1,5 @@
+let datespan = document.querySelector('#currentYear')
+datespan.innerText = new Date().getFullYear()
+
+let lastmodifiedspan = document.querySelector("#lastModified")
+lastmodifiedspan.innerText = document.lastModified
