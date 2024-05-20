d43b0c6fb0efd5f110cde5b585c2858e592d9439
Peyton White <whi20027@byui.edu>
2024-05-20 13:35:55 -0600

    style

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -3,3 +3,10 @@ datespan.innerText = new Date().getFullYear()
 
 let lastmodifiedspan = document.querySelector("#lastModified")
 lastmodifiedspan.innerText = document.lastModified
+
+let menu_button = document.getElementById("menu-button")
+let navlist = document.querySelector("nav ul")
+menu_button.addEventListener('click', () =>{
+    menu_button.classList.toggle("open")
+    navlist.classList.toggle("open")
+})
