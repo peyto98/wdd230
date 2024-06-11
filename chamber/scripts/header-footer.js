0ef9a7b594761046681c35566ab0253a494385cd
Peyton White <whi20027@byui.edu>
2024-06-11 13:54:59 -0600

    responsive

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -4,9 +4,31 @@ datespan.innerText = new Date().getFullYear()
 let lastmodifiedspan = document.querySelector("#lastModified")
 lastmodifiedspan.innerText = document.lastModified
 
-let menu_button = document.getElementById("menu-button")
+let menu_button = document.getElementById("menubutton")
 let navlist = document.querySelector("nav ul")
 menu_button.addEventListener('click', () =>{
     menu_button.classList.toggle("open")
     navlist.classList.toggle("open")
+})
+
+let darkbutton = document.getElementById("dark-mode")
+darkbutton.addEventListener("click", () =>{    
+    darkbutton.classList.toggle("dark")
+    if (darkbutton.classList.contains("dark")){
+        document.documentElement.style.setProperty('--text-color', 'white');        
+        document.documentElement.style.setProperty('--inv-text-color', 'black');        
+        document.documentElement.style.setProperty('--background-color', '#1f1f1d');    
+        document.documentElement.style.setProperty('--card-color', '#588157');        
+        document.documentElement.style.setProperty('--header-color', '#3A5A40');    
+        document.documentElement.style.setProperty('--nav-color', '#1e2f21');                
+    }
+    else{
+        document.documentElement.style.setProperty('--text-color', 'black');        
+        document.documentElement.style.setProperty('--inv-text-color', 'white');        
+        document.documentElement.style.setProperty('--background-color', '#C2B280');    
+        document.documentElement.style.setProperty('--card-color', '#A3B18A');        
+        document.documentElement.style.setProperty('--header-color', '#588157');    
+        document.documentElement.style.setProperty('--nav-color', '#3A5A40');                
+    }
+
 })
