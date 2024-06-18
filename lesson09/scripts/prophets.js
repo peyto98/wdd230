ca1fd09412b6cb5b0ad4029dda890b943e14532d
Peyton White <whi20027@byui.edu>
2024-06-18 15:53:16 -0600

    prophets

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,28 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let newsection = document.createElement("section");
+        newsection.innerHTML = `
+                <h2>${prophet.name} ${prophet.lastname}</h2>
+                <img src="${prophet.imageurl}" alt="${prophet.name} ${prophet.lastname}" loading="lazy" height="400">`
+                cards.append(newsection)
+            }
+
+);
+  }
+
+async function getProphetData(){
+    const response = await fetch(url)
+    if (response.ok){
+        const data = await response.json()
+        // console.table(data)
+        displayProphets(data.prophets)
+    }
+    else{
+        console.log("This does not work.")
+    }
+}
+
+getProphetData()
